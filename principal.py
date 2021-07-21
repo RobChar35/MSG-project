ee76e40e8291b96202754804e3da8b42e8867d94
Robert Charca <trevoraobac@gmail.com>
2021-07-21 14:53:39 -0500

    Making progress with the generator.

--- principal.py
+++ principal.py
@@ -1,10 +1,9 @@
 import random
 
 #Java function
-def java():
-    pass
-
-#Bedrock function
-def bedrock():
-    pass
-
+def java_or_bedrock(limit):
+    symbol = " -"
+    for i in range(limit):
+        print(9, end="")    
+    print(random.choice(symbol), random.randint(10000, 9999999999999999999), end="")
+java_or_bedrock(10)
