c9d50f504f28f70800d6b4f3bc9b615b2a098c14
Robert Charca <trevoraobac@gmail.com>
2021-07-22 19:21:44 -0500

    Simple menu in progress...

--- principal.py
+++ principal.py
@@ -7,4 +7,5 @@ def java_or_bedrock(limit, start, end):
     for i in range(limit):
         random_number = random.randint(start, end)
         print(random_number, end="")
-java_or_bedrock(19, 1, 9)
+
+#Simple menu
