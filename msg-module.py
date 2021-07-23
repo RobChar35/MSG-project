cbfcace21ed5216c301c192a4e102860936777d6
Robert Charca <trevoraobac@gmail.com>
2021-07-22 21:25:53 -0500

    New functions in msg-module and principal function moved to the new file.

---
+++ msg-module.py
@@ -0,0 +1,23 @@
+import random
+
+#Java and bedrock function
+def java_or_bedrock(limit, start, end):
+    symbols = random.choice(" -")
+    print(symbols, end="")
+    for i in range(limit):
+        random_number = random.randint(start, end)
+        print(random_number, end="")
+
+#Your own election
+def selection():
+    limit_s = int(input("Write a limit: "))
+    start_s = int(input("Write the number you want to begin: "))
+    end_s = int(input("Write the number you want to end: "))
+    java_or_bedrock(limit_s, start_s, end_s) 
+
+#Random values in java_or_bedrock function
+def get_lucky():
+    limit_gl = random.randint(5, 19)
+    start_gl = random.randint(1, 9)
+    end_gl = random.randint(1, 9)
+    java_or_bedrock(limit_gl, start_gl, end_gl)
--- principal.py
+++ principal.py
@@ -1,11 +1,3 @@
 import random
 
-#Java and bedrock function
-def java_or_bedrock(limit, start, end):
-    symbols = random.choice(" -")
-    print(symbols, end="")
-    for i in range(limit):
-        random_number = random.randint(start, end)
-        print(random_number, end="")
-
 #Simple menu
