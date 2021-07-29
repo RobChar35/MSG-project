776ea8e887a8b59089e5d50c22cb0e81cb4ba00d
Robert Charca <trevoraobac@gmail.com>
2021-07-29 11:50:18 -0500

    msg-module changed to msg_module.

---
+++ msg_module.py
@@ -0,0 +1,28 @@
+import random
+
+#Java and bedrock function
+def java_or_bedrock(limit, start, end):
+    symbols = random.choice(" -")
+    print(symbols, end="")
+    for i in range(limit):
+        random_number = random.randint(start, end)
+        print(random_number, end="")
+
+#Your own choice
+def selection():
+    limit_s = int(input("Write a limit: "))
+    start_s = int(input("Write the number you want to begin: "))
+    end_s = int(input("Write the number you want to end: "))
+     
+    if limit_s == 0:
+        print("ERROR: You must define a limit greater than 0.")
+    else:
+        java_or_bedrock(limit_s, start_s, end_s)
+
+#Random values in java_or_bedrock function
+def get_lucky():
+    limit_gl = random.randrange(5, 19, 1)
+    start_gl = random.randrange(0, 9, 1)
+    end_gl = random.randrange(0, 9, 1)
+    java_or_bedrock(limit_gl, start_gl, end_gl)
+get_lucky()
--- principal.py
+++ principal.py
@@ -1,3 +1,12 @@
+from msg_module import *
 import random
 
 #Simple menu
+option = str(input("Write something: "))
+while option is True:
+    if option == "Customize":
+        selection()
+    elif option == "random":
+        get_lucky()
+    else:
+        print("Error.")
