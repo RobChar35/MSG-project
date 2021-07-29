887617fc548da08412cac6ca07de1663ba928944
Robert Charca <trevoraobac@gmail.com>
2021-07-28 23:11:06 -0500

    Conditional added inside of thw function called 'selection'. Doing some testing in the function 'get lucky'

--- msg-module.py
+++ msg-module.py
@@ -8,16 +8,21 @@ def java_or_bedrock(limit, start, end):
         random_number = random.randint(start, end)
         print(random_number, end="")
 
-#Your own election
+#Your own choice
 def selection():
     limit_s = int(input("Write a limit: "))
     start_s = int(input("Write the number you want to begin: "))
     end_s = int(input("Write the number you want to end: "))
-    java_or_bedrock(limit_s, start_s, end_s) 
+     
+    if limit_s == 0:
+        print("ERROR: You must define a limit greater than 0.")
+    else:
+        java_or_bedrock(limit_s, start_s, end_s)
 
 #Random values in java_or_bedrock function
 def get_lucky():
     limit_gl = random.randint(5, 19)
-    start_gl = random.randint(1, 9)
-    end_gl = random.randint(1, 9)
+    start_gl = random.randint(0, 9)
+    end_gl = random.randint(0, 9)
     java_or_bedrock(limit_gl, start_gl, end_gl)
+get_lucky()
