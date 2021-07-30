f9ccb1556a89be57180d9d5310a809693d6e9a22
Robert Charca <trevoraobac@gmail.com>
2021-07-30 18:31:18 -0500

    Simple menu done.

--- principal.py
+++ principal.py
@@ -1,12 +1,10 @@
 from msg_module import *
-import random
 
 #Simple menu
 option = str(input("Write something: "))
-while option is True:
-    if option == "Customize":
-        selection()
-    elif option == "random":
-        get_lucky()
-    else:
-        print("Error.")
+if option == "Customize":
+    selection()
+elif option == "random":
+    get_lucky()
+else:
+    print("Error.")
